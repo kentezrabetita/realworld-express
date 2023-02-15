8c563c91dd538ae453253df0565a8954afbf3051
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-02-15 17:53:27 +0800

    ✨ feat: add article model

---
+++ src/models/article.model.ts
@@ -0,0 +1,26 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import db from '../config/database.js';
+
+const Article = db.define('article', {
+  slug: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT
+  },
+  body: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+});
+
+export default Article;
+
+// Article.beforeValidate((article) => {
+//   article.slug = slugify(article.title, { lower: true }); // ! will require slugify library
+// });
