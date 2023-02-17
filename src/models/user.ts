74511313c9dc6f876a281ae91b1f2900e6bba10e
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-02-17 21:07:33 +0800

    ✨ feat: update user model atrributes

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,22 +1,31 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import db from '../config/database.js';
 
-const User = db.define(
-  'user',
-  {
-    name: {
-      type: DataTypes.STRING
-    },
-    description: {
-      type: DataTypes.STRING
-    },
-    status: {
-      type: DataTypes.BOOLEAN
-    }
+const User = db.define('user', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
   },
-  {
-    freezeTableName: true
+
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  bio: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  image: {
+    type: DataTypes.TEXT,
+    allowNull: true
   }
-);
+});
 
 export default User;
