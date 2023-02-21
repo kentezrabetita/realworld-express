f4d4a28bff8f104f63a5cd468a8c66dc438aa91d
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-02-21 21:31:21 +0800

    ✨ feat: add route for updating and deleting article

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import {
   getAllArticlesHandler,
   getArticleHandler,
-  createArticleHandler
+  createArticleHandler,
+  updateArtilceHandler,
+  deleteArticleHandler
 } from '../controllers/article.js';
 
 const router = express.Router();
@@ -11,5 +13,8 @@ router.get('/', getAllArticlesHandler);
 router.get('/:id', getArticleHandler);
 
 router.post('/', createArticleHandler);
+
+router.patch('/:id', updateArtilceHandler);
+router.delete('/:id', deleteArticleHandler);
 
 export default router;
