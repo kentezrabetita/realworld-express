e304c23d4e86ac07fa747398872bbb674a2f58ba
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-02-21 21:30:22 +0800

    ✨ feat: add handler for updating and deleting article

--- src/controllers/article.ts
+++ src/controllers/article.ts
@@ -27,3 +27,32 @@ export const createArticleHandler = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const updateArtilceHandler = async (req: Request, res: Response) => {
+  try {
+    const article = await ArticleService().updateArticle(
+      Number(req.params.id),
+      req.body
+    );
+    if (article === null) {
+      res.status(404).json({ message: 'Article not found!' });
+    } else {
+      res.json({ message: 'Article Updated' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteArticleHandler = async (req: Request, res: Response) => {
+  try {
+    const article = await ArticleService().deleteArticle(Number(req.params.id));
+    if (article === null) {
+      res.status(404).json({ message: 'Article not found' });
+    } else {
+      res.json({ message: 'Article Deleted' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
