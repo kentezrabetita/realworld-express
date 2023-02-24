38600d21a3abddb827b6c52ca75a15dd49ff0266
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-02-24 23:56:39 +0800

    🧹 chore: add alert comment in db config

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER!,
   process.env.DB_PASSWORD!,
   {
+    // ! use process.env.PORT when not using docker compose
     host: process.env.DB_HOST!,
     dialect: 'postgres',
     logging: false
